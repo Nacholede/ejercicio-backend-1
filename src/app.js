45fa7308e854c0110dfc93d7e08da0a8d1b7ba74
ignacio ledesma <nacholede013@gmail.com>
2023-01-19 22:24:49 -0300

    Primer pre-entrega

--- src/app.js
+++ src/app.js
@@ -1,31 +1,23 @@
-import express from 'express' 
-import ProductManager from '../productManager'
-const productManager = new ProductManager ('./productManager.js')
-const app = express ()
+import express from 'express'
+import productsRouter from '../routes/products.router.js'
+import cartRouter from '../routes/cart.router.js'
+
+const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname+'/public'))
 
-PORT = 8080
 
-app.get('/products' , async(req,res) => {
-    try {
-        const {limit} = req.query 
-        console.log (limit)
-        const productos = await ProductManager.getProducts ()
-        res.json ({productos})
+app.use('/api/products', productsRouter)
+app.use('/api/cart', cartRouter)
 
-    } catch (error) {
-        res.send (error)
-    }
+app.get('/',(req,res)=>{
+    res.send('Ruta raiz')
 })
 
-app.get ('/products/:pid', async (req,res) => {
-    const {idProduct} = req.params 
-    const product = await ProductManager.getProductById(parseInt(idProduct))
-    if (product) {
-        res.status (200).json ({message: 'Producto encontrado con exito' , product})
-    } else {
-        res.status (400).json ({error: 'Producto no encontrado'})
-    }
-} )
+const PORT = 8080
+
+app.listen(PORT, () => {
+  console.log(`Escuchando al puerto ${PORT}`)
+})
